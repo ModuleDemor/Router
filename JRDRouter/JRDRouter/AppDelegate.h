770e8d7e6b5a347412ee0461051c19c5e793815b
taowang <taowang@tcl.com>
2018-09-04 17:33:18 +0800

    add new

---
+++ JRDRouter/JRDRouter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JRDRouter
+//
+//  Created by 王涛 on 2018/9/4.
+//  Copyright © 2018年 tcl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
